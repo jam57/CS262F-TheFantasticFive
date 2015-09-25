c70033401d04530b7d23963c59596398ad9f3190
David Michel <davejoshmike@gmail.com>
2015-09-24 22:42:48 -0400

    First Prototype
    Climbing Log First Prototype

---
+++ ClimbingLog/app/src/main/java/dmichel/ClimbingLog/MainActivity.java
@@ -0,0 +1,27 @@
+package dmichel.ClimbingLog;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        android.support.v7.app.ActionBar ab = getSupportActionBar();
+        ab.setDisplayShowHomeEnabled(true);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_activity_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+}
